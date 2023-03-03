56a98d8d56fc8d8c9772de0748fd2c73b3f34a6b
Martin <martin.rialhe-badet@etu.univ-savoie.fr>
2023-03-03 09:15:33 +0100

    Trying to look at the columns' information

--- LOL.py
+++ LOL.py
@@ -9,9 +9,13 @@ matches = pd.read_csv('matches.csv', index_col=0)
 # Print the first 5 rows of the champions data
 print(champion.head())
 #plot the data
-champion.plot()
+champion.plot() # Not very helpful
 plt.figure()
-plt.plot(champion['attack'], champion['magic'], 'o')
+plt.plot(champion['attack'], champion['magic'], 'o') # Not very helpful either
 plt.xlabel('attack')
 plt.ylabel('magic')
+# Plot the mean and standard deviation of the numerical columns
+champion.plot(kind='box') # Not very helpful
+# Print the mean and standard deviation of the numerical columns
+print(champion.describe())
 plt.show()
