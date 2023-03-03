07937c46bf84594c712c488161bc09d52c7b91af
Martin <martin.rialhe-badet@etu.univ-savoie.fr>
2023-03-03 10:07:15 +0100

    Commentaires et affichages en français

--- LOL.py
+++ LOL.py
@@ -1,61 +1,66 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# Read in the data
+# Lire les données
 champion = pd.read_csv('champions.csv', index_col=0)
-# Tags are still a string, so we need to make them a list
+# Les tags sont encore sous forme de chaîne de caractères, nous devons les convertir en listes
 champion['tags'] = champion['tags'].apply(lambda x: x.strip('[]').split(', '))
 matches = pd.read_csv('matches.csv', index_col=0)
-# Print the first 5 rows of the champions data
+# Afficher les 5 premières lignes des données sur les champions
 print(champion.head())
-#plot the data
-champion.plot() # Not very helpful
+
+# Tracer les données
+champion.plot() # Pas très utile
 plt.figure()
-plt.plot(champion['attack'], champion['magic'], 'o') # Not very helpful either
-plt.xlabel('attack')
-plt.ylabel('magic')
-# Plot the mean and standard deviation of the numerical columns (there are a lot so we need the x axis legend to be sideways)
-champion.plot(kind='box', title='Mean and standard deviation of the numerical columns') # Not very helpful
+plt.plot(champion['attack'], champion['magic'], 'o') # Pas très utile non plus
+plt.xlabel('attaque')
+plt.ylabel('magie')
+
+# Tracer la moyenne et l'écart type des colonnes numériques (il y en a beaucoup, donc nous devons mettre la légende de l'axe des x de côté)
+champion.plot(kind='box', title='Moyenne et écart type des colonnes numériques') # Pas très utile
 plt.xticks(rotation=45)
-# Print the mean and standard deviation of the numerical columns
+
+# Afficher la moyenne et l'écart type des colonnes numériques
 print(champion.describe())
-# Let's try to look at how stats correlate with tags
-# First, let's look at the tags
-# Tags are stored as a list of strings, let's get a list of all the tags
+
+# Essayons de voir comment les statistiques sont corrélées aux tags
+# Tout d'abord, regardons les tags
+# Les tags sont stockés sous forme de liste de chaînes de caractères, obtenons une liste de tous les tags
 tags = []
 for tag in champion['tags']:
     tags.extend(tag)
-# Now let's show the number of champions with each tag
-plt.figure("Number of champions with each tag")
+# Montrons maintenant le nombre de champions avec chaque tag
+plt.figure("Nombre de champions avec chaque tag")
 plt.hist(tags)
 plt.xlabel('Tag')
-plt.ylabel('Number of champions')
-# Show the correlation between tags and roles
-# Roles are bluetop, bluejungle, bluemid, blueadc, bluesupport, redtop, redjungle, redmid, redadc, redsupport and are columns in the matches data
-# First, let's get a list of all the roles
+plt.ylabel('Nombre de champions')
+
+# Montrons la corrélation entre les tags et les rôles
+# Les rôles sont "bluetop", "bluejungle", "bluemid", "blueadc", "bluesupport", "redtop", "redjungle", "redmid", "redadc", "redsupport" et sont des colonnes dans les données de match
+# Tout d'abord, obtenons une liste de tous les rôles
 roles = matches.columns[3:-1]
-# Now let's get a list of all the unique tags
+# Maintenant, obtenons une liste de tous les tags uniques
 unique_tags = list(set(tags))
-# Now let's make a dataframe with the number of champions with each tag for each role
+# Maintenant, créons un dataframe avec le nombre de champions ayant chaque tag pour chaque rôle
 tag_role = pd.DataFrame(index=unique_tags, columns=roles)
 for role in roles:
     for tag in unique_tags:
         tag_role.loc[tag, role] = sum(matches[role].isin(champion[champion['tags'].apply(lambda x: tag in x)].index))
-# Regroup the blue and red roles
+# Regroupons les rôles bleus et rouges
 tag_role['top'] = tag_role['bluetop'] + tag_role['redtop']
 tag_role['jungle'] = tag_role['bluejungle'] + tag_role['redjungle']
 tag_role['mid'] = tag_role['bluemid'] + tag_role['redmid']
 tag_role['adc'] = tag_role['blueadc'] + tag_role['redadc']
 tag_role['support'] = tag_role['bluesupport'] + tag_role['redsupport']
 tag_role = tag_role.drop(['bluetop', 'bluejungle', 'bluemid', 'blueadc', 'bluesupport', 'redtop', 'redjungle', 'redmid', 'redadc', 'redsupport'], axis=1)
-# flip the dataframe so that the roles are the index
+# Renversons le dataframe pour que les rôles soient les lignes et les tags les colonnes
 tag_role = tag_role.transpose()
-# Now let's plot the data
-tag_role.plot(kind='bar', stacked=True, title='Number of champions with each tag for each role')
-plt.xlabel('Role')
-plt.ylabel('Number of champions')
+# Tracer le résultat
+tag_role.plot(kind='bar', stacked=True, title='Nombre de champions avec chaque tag pour chaque rôle')
+plt.xlabel('Rôle')
+plt.ylabel('Nombre de champions')
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-# Now let's look at the correlation between stats and roles
+# Montrons maintenant la corrélation entre les tags et les statistiques
+# Les stats sont attack, defense, magic, difficulty, hp, hpperlevel, mp, mpperlevel, movespeed, armor, armorperlevel, spellblock, spellblockperlevel, attackrange, hpregen, hpregenperlevel, mpregen, mpregenperlevel, crit, critperlevel, attackdamage, attackdamageperlevel, attackspeedperlevel, attackspeed
 plt.show()
