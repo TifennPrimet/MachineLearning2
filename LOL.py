afdb1c82a6990c4796dfb793433c63c19fff1283
Martin <martin.rialhe-badet@etu.univ-savoie.fr>
2023-03-03 09:33:05 +0100

    Process tags correctly

--- LOL.py
+++ LOL.py
@@ -5,6 +5,8 @@ import numpy as np
 
 # Read in the data
 champion = pd.read_csv('champions.csv', index_col=0)
+# Tags are still a string, so we need to make them a list
+champion['tags'] = champion['tags'].apply(lambda x: x.strip('[]').split(', '))
 matches = pd.read_csv('matches.csv', index_col=0)
 # Print the first 5 rows of the champions data
 print(champion.head())
@@ -18,4 +20,15 @@ plt.ylabel('magic')
 champion.plot(kind='box') # Not very helpful
 # Print the mean and standard deviation of the numerical columns
 print(champion.describe())
+# Let's try to look at how stats correlate with tags
+# First, let's look at the tags
+# Tags are stored as a list of strings, let's get a list of all the tags
+tags = []
+for tag in champion['tags']:
+    tags.extend(tag)
+# Now let's show the number of champions with each tag
+plt.figure()
+plt.hist(tags)
+plt.xlabel('Tag')
+plt.ylabel('Number of champions')
 plt.show()
