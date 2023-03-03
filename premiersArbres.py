1b462dbe12f0e6d123e5057ebc50dbe7830350ec
TifennPrimet <94844547+TifennPrimet@users.noreply.github.com>
2023-03-03 16:05:31 +0100

    create premier arbre

---
+++ premiersArbres.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+if True: # Lecture des données
+    # Lire les données
+    champion = pd.read_csv('champions.csv', index_col=None)
+    # Les tags sont encore sous forme de chaîne de caractères, nous devons les convertir en listes
+    champion['tags'] = champion['tags'].apply(lambda x: x.strip('[]').split(', '))
+    matches = pd.read_csv('matches.csv', index_col=None)
