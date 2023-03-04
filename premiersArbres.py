31c27fdbf213c3f9a515dd354546ccfb00aa9795
Martin <martin.rialhe-badet@etu.univ-savoie.fr>
2023-03-04 16:35:31 +0100

    Modificitaion des helpers pour permettre d'avoir plusieurs stats

--- premiersArbres.py
+++ premiersArbres.py
@@ -12,19 +12,20 @@ if True: # Lecture des données
     matches = pd.read_csv('matches.csv', index_col=None)
 
 # Helper functions
-def getStats(champions, stat):
-    # Cette fonction permet de récupérer les statistiques d'un champion
+def getStat(color, role, stat):
+    # Cette fonction permet de récupérer les statistiques du champion qui a joué le rôle donné pour l'équipe donnée
     # Entrée:
-    #   - champions: la liste des champions
+    #   - color: la couleur de l'équipe
+    #   - role: le rôle du champion
     #   - stat: la statistique à récupérer
     # Sortie:
     #   - stats: une liste contenant la statistique pour chaque champion
     stats = []
-    for champ in champions:
+    for champ in matches[color + role]:
         stats.append(champion[champion['id'] == champ][stat].values[0])
     return stats
 
-def train_test_split(func: callable, test_size: float=0.2):
+def train_test_split(func: callable, *args, test_size: float=0.2):
     # Cette fonction permet de diviser les données en un ensemble d'entraînement et un ensemble de test
     # Entrée:
     #   - func: la fonction qui permet de récupérer les données
@@ -34,7 +35,7 @@ def train_test_split(func: callable, test_size: float=0.2):
     #   - X_test: les données de test
     #   - y_train: les labels d'entraînement
     #   - y_test: les labels de test
-    X = [[i, j] for i, j in zip(func('blue'), func('red'))]
+    X = [list(i) for i in zip(*sum([[func('red', arg[0], ar) for ar in arg[1]] for arg in args] + [[func('blue', arg[0], ar) for ar in arg[1]] for arg in args], []))] # Si ça marche, pas touche
     y = matches['result']
     n = len(X)
     cut = int((1-test_size)*n)
@@ -71,7 +72,7 @@ def train(X_train: list, y_train: list, min_samples_split: int=2, max_depth: int
     clf = clf.fit(X_train, y_train)
     return clf
 
-def bestParams(func: callable, test_size: float=0.2, min_samples_split: list=[2, 5, 10, 20, 50, 100], max_depth: list=[None, 2, 5, 10, 20, 50, 100]):
+def bestParams(func: callable, *args, test_size: float=0.2, min_samples_split: list=[2, 5, 10, 20, 50, 100], max_depth: list=[None, 2, 5, 10, 20, 50, 100]):
     # Cette fonction permet de trouver les meilleurs paramètres pour un classifieur
     # Entrée:
     #   - func: la fonction qui permet de récupérer les données
@@ -83,7 +84,7 @@ def bestParams(func: callable, test_size: float=0.2, min_samples_split: list=[2,
     bestParams = {'min_samples_split': 0, 'max_depth': 0, 'accuracy': 0}
     for i in min_samples_split:
         for j in max_depth:
-            X_train, X_test, y_train, y_test = train_test_split(func, test_size)
+            X_train, X_test, y_train, y_test = train_test_split(func, *args, test_size=test_size)
             clf = train(X_train, y_train, i, j)
             accuracy = getAccuracy(clf, X_test, y_test)
             # print('min_samples_split =', i, 'max_depth =', j, 'accuracy =', accuracy)
@@ -93,7 +94,7 @@ def bestParams(func: callable, test_size: float=0.2, min_samples_split: list=[2,
                 bestParams['accuracy'] = accuracy
     return bestParams
 
-def bestParamsplot(func: callable, test_size: float=0.2, min_samples_split: list=[2, 5, 10, 20, 50, 100], max_depth: list=[None, 2, 5, 10, 20, 50, 100]):
+def bestParamsplot(func: callable, *args, test_size: float=0.2, min_samples_split: list=[2, 5, 10, 20, 50, 100], max_depth: list=[None, 2, 5, 10, 20, 50, 100]):
     # Cette fonction permet de trouver les meilleurs paramètres pour un classifieur et d'afficher une heatmap
     # Entrée:
     #   - func: la fonction qui permet de récupérer les données
@@ -107,7 +108,7 @@ def bestParamsplot(func: callable, test_size: float=0.2, min_samples_split: list
     for i in min_samples_split:
         accuracy.append([])
         for j in max_depth:
-            X_train, X_test, y_train, y_test = train_test_split(func, test_size)
+            X_train, X_test, y_train, y_test = train_test_split(func, *args, test_size=test_size)
             clf = train(X_train, y_train, i, j)
             accuracy[-1].append(getAccuracy(clf, X_test, y_test))
             # print('min_samples_split =', i, 'max_depth =', j, 'accuracy =', accuracy[-1][-1])
@@ -122,18 +123,25 @@ def bestParamsplot(func: callable, test_size: float=0.2, min_samples_split: list
     plt.show()
     return bestParams
 
-# # Exemple d'utilisation des fonctions
-# X_train, X_test, y_train, y_test = train_test_split(lambda color: getStats(matches[color + 'top'],'hp'), 0.2)
-# clf = train(X_train, y_train, 100, 5)
-# print(getAccuracy(clf, X_test, y_test)) # 0.5125786163522013, pas super mais mieux que rien
+# Exemple d'utilisation des fonctions
+X_train, X_test, y_train, y_test = train_test_split(getStat, ('top', ('hp',)), test_size=0.2)
+clf = train(X_train, y_train, 100, 5)
+print(getAccuracy(clf, X_test, y_test)) # 0.5125786163522013, pas super mais mieux que rien
 
-# # bestParams automatise la recherche des meilleurs paramètres (min_samples_split et max_depth)
-# params = bestParams(lambda color: getStats(matches[color + 'top'],'hp'), 0.2)
-# print(params)
+# bestParams automatise la recherche des meilleurs paramètres (min_samples_split et max_depth)
+params = bestParams(getStat, ('top', ('hp',)), test_size=0.2)
+print(params)
 
 # bestParamsplot affiche aussi une heatmap des résultats
-params = bestParamsplot(lambda color: getStats(matches[color + 'top'],'hp'), 0.2)
+params = bestParamsplot(getStat, ('top', ('hp',)), test_size=0.2)
 print(params)
+
+# # Les hps seul ne sont pas très efficaces, on peut essayer avec les hps et les mps
+# X_train, X_test, y_train, y_test = train_test_split(getStat, ('top', ('hp', 'mp')), test_size=0.2)
+# print(X_test)
+
+# params = bestParamsplot(getStat, ('top', ('hp', 'mp')), test_size=0.2)
+# print(params)
 
 # print(matches['bluetop'])
 # clf = tree.DecisionTreeClassifier(min_samples_split=100, max_depth=5)
