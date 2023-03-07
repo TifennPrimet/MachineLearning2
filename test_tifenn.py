811e6d323d06c3a0fdba75b56f3f604073a48b49
Martin <martin.rialhe-badet@etu.univ-savoie.fr>
2023-03-07 14:17:16 +0100

    ratio_victoire pour Tifenn

--- test_tifenn.py
+++ test_tifenn.py
@@ -57,7 +57,7 @@ def ajoute_ratio_victoire(data):
     """
     Ajoute une colonne au dataframe data avec le ratio de victoire de chaque equipe
     """
-    victoire = pd.read_csv('data_victoire.csv', index_col=None)
+    victoire = ratio_victoire(pd.read_csv('data_victoire.csv', index_col=None))
     # On ajoute le ratio de victoire de l'equipe bleu et de l'equipe bleu dans le dataframe data
     data['ratio_victoire_blueteam'] = 0
     data['ratio_victoire_redteam'] = 0
@@ -114,20 +114,20 @@ if __name__ == '__main__' :
 
     ajout_full_stat('new_full_stats.csv',matches)
     # on lit les donnees de full_stats.csv
-    stats = pd.read_csv('full_stats.csv', index_col=None)
+    # stats = pd.read_csv('full_stats.csv', index_col=None)
 
-    new_datas = pd.read_csv('new_full_stats.csv', index_col=None)
-    print(new_datas.head())
-    # prend juste les ratio de victoire et la difference de ratio de victoire
-    data, result = prepare_donnee(getStat_ratio_victoire, ('blue', ('nimportequoi',)), ('red', ('nimportequoi',)))
-    X_train, X_test, y_train, y_test = train_test_split(data, result, test_size=0)
-    clf = train(X_train, y_train, 2, 5) # entrine l'arbre de decision
+    # new_datas = pd.read_csv('new_full_stats.csv', index_col=None)
+    # print(new_datas.head())
+    # # prend juste les ratio de victoire et la difference de ratio de victoire
+    # data, result = prepare_donnee(getStat_ratio_victoire, ('blue', ('nimportequoi',)), ('red', ('nimportequoi',)))
+    # X_train, X_test, y_train, y_test = train_test_split(data, result, test_size=0)
+    # clf = train(X_train, y_train, 2, 5) # entrine l'arbre de decision
 
-    fig, ax = plt.subplots()
-    tree.plot_tree(clf, feature_names = data.columns, class_names=['red', 'blue'])
-    plt.savefig('Tifenn/tree_ratio_victoire.pgf') # enregistrement .pgf pour pouvoir zoomer
+    # fig, ax = plt.subplots()
+    # tree.plot_tree(clf, feature_names = data.columns, class_names=['red', 'blue'])
+    # plt.savefig('Tifenn/tree_ratio_victoire.pgf') # enregistrement .pgf pour pouvoir zoomer
 
-    print(classe(clf, new_datas) )# classe les nouvelles donnees
+    # print(classe(clf, new_datas) )# classe les nouvelles donnees
     # print("accuracy = ", getAccuracy(clf, X_test, y_test)) # 0.5293501048218029 avec 2 3
     # plt.show()
     # traceMatriceConf(clf, X_test, y_test)
