0aa0f4ea4f4808e9563ab1f37e9a50e1fd6703e0
Rita Abou-Samra <Rita.Abou-Samra@etu.univ-savoie.fr>
2023-03-07 09:47:08 +0100

    modified test_rita.py

--- test_rita.py
+++ test_rita.py
@@ -20,15 +20,22 @@ y_test = pickle.load(open('pkl_rita/test4_y_test.pkl', 'rb'))
 #best = bestParamsplot(X_train, X_test, y_train, y_test, range(2, 70), range(2, 70)) # donne samples = 2 et depth = 2
 #print('Le meilleur paramètre =', best)
 # On fait l'arbre
-#arbre = train(X_train,y_train,19,13) # samples = 19 et depth = 13
-#pickle.dump(arbre, open('pkl_rita/test4_tree.pkl', 'wb'))
-arbre = pickle.load(open('pkl_rita/test4_tree.pkl', 'rb'))
-tree.plot_tree(arbre,feature_names=X.columns,class_names=['red','blue'])
+arbre_gini = train(X_train,y_train,19,13,'gini') # samples = 19 et depth = 13
+pickle.dump(arbre_gini, open('pkl_rita/test4_tree.pkl', 'wb'))
+arbre_gini = pickle.load(open('pkl_rita/test4_tree.pkl', 'rb'))
+tree.plot_tree(arbre_gini,feature_names=X.columns,class_names=['red','blue'])
+arbre_entropy = train(X_train,y_train,19,13,'entropy') # samples = 19 et depth = 13
+pickle.dump(arbre_entropy, open('pkl_rita/test4_tree.pkl', 'wb'))
+arbre_entropy = pickle.load(open('pkl_rita/test4_tree.pkl', 'rb'))
+tree.plot_tree(arbre_entropy,feature_names=X.columns,class_names=['red','blue'])
 #plt.show()
-# On calcule la précision (0.5618448637316562)
-acc = getAccuracy(arbre,X_test,y_test)
-print('La precision est de ', acc*100, '% pour le test 4.')
-# La precision est de  56.81341719077568 % pour le test 4.
+# On calcule la précision (0.)
+acc_gini = getAccuracy(arbre_gini,X_test,y_test)
+acc_entropy = getAccuracy(arbre_entropy,X_test,y_test)
+print('La precision avec gini est de ', acc_gini*100, '% pour le test 4.')
+print('La precision avec entropy est de ', acc_entropy*100, '% pour le test 4.')
+#La precision avec gini est de  56.39412997903563 % pour le test 4.
+#La precision avec entropy est de  54.926624737945495 % pour le test 4.
 
 #test 5 :
 # On entraine 80% des données
@@ -53,7 +60,7 @@ y_test = pickle.load(open('pkl_rita/test5_y_test.pkl', 'rb'))
 arbre_gini = train(X_train,y_train,7,26,'gini') # samples = 7 et depth = 26
 pickle.dump(arbre_gini, open('pkl_rita/test5_tree.pkl', 'wb'))
 arbre_gini = pickle.load(open('pkl_rita/test5_tree.pkl', 'rb'))
-tree.plot_tree(arbre,feature_names=X.columns,class_names=['red','blue'])
+tree.plot_tree(arbre_gini,feature_names=X.columns,class_names=['red','blue'])
 arbre_entropy = train(X_train,y_train,7,26,'entropy') # samples = 7 et depth = 26
 pickle.dump(arbre_entropy, open('pkl_rita/test5_tree.pkl', 'wb'))
 arbre_entropy = pickle.load(open('pkl_rita/test5_tree.pkl', 'rb'))
@@ -67,3 +74,6 @@ print('La precision avec entropy est de ', acc_entropy*100, '% pour le test 5.')
 # La precision avec gini est de 54.29769392033543 % pour le test 5 avec gini.
 # La precision avec entropy est de 52.30607966457023 % pour le test 5 avec entropy.
 
+
+#test 6 :
+# On entraine 80% des données
