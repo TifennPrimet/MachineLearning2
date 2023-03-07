6574ca83115cb002994b575fbee40996df769ca5
Rita Abou-Samra <Rita.Abou-Samra@etu.univ-savoie.fr>
2023-03-07 10:04:37 +0100

    modified test_rita

--- test_rita.py
+++ test_rita.py
@@ -77,3 +77,39 @@ print('La precision avec entropy est de ', acc_entropy*100, '% pour le test 5.')
 
 #test 6 :
 # On entraine 80% des données
+
+#On sépare le jeu de données
+#X, y = prepare_donnee(getStat_red_blue, ('top', ('hp','armor','attack')), ('jungle', ('attack','movespeed',)), ('mid',('magic',)), ('support',('hp',)))
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# On enregistre la séparation
+#pickle.dump(X_train, open('pkl_rita/test6_X_train.pkl', 'wb'))
+#pickle.dump(X_test, open('pkl_rita/test6_X_test.pkl', 'wb'))
+#pickle.dump(y_train, open('pkl_rita/test6_y_train.pkl', 'wb'))
+#pickle.dump(y_test, open('pkl_rita/test6_y_test.pkl', 'wb'))
+
+X_train = pickle.load(open('pkl_rita/test6_X_train.pkl', 'rb'))
+X_test = pickle.load(open('pkl_rita/test6_X_test.pkl', 'rb'))
+y_train = pickle.load(open('pkl_rita/test6_y_train.pkl', 'rb'))
+y_test = pickle.load(open('pkl_rita/test6_y_test.pkl', 'rb'))
+
+# On calcule les meilleurs paramètres
+#best = bestParamsplot(X_train, X_test, y_train, y_test, range(2, 45), range(2, 45)) # donne samples = 2 et depth = 2
+#print('Le meilleur paramètre =', best)
+# On fait l'arbre
+arbre_gini = train(X_train,y_train,2,6,'gini') # samples = 2 et depth = 6
+pickle.dump(arbre_gini, open('pkl_rita/test6_tree.pkl', 'wb'))
+arbre_gini = pickle.load(open('pkl_rita/test6_tree.pkl', 'rb'))
+tree.plot_tree(arbre_gini,feature_names=X.columns,class_names=['red','blue'])
+arbre_entropy = train(X_train,y_train,2,6,'entropy') # samples = 2 et depth = 6
+pickle.dump(arbre_entropy, open('pkl_rita/test6_tree.pkl', 'wb'))
+arbre_entropy = pickle.load(open('pkl_rita/test6_tree.pkl', 'rb'))
+tree.plot_tree(arbre_entropy,feature_names=X.columns,class_names=['red','blue'])
+plt.show()
+# On calcule la précision (0.5461215932914046)
+acc_gini = getAccuracy(arbre_gini,X_test,y_test)
+acc_entropy = getAccuracy(arbre_entropy,X_test,y_test)
+print('La precision avec gini est de ', acc_gini*100, '% pour le test 6.')
+print('La precision avec entropy est de ', acc_entropy*100, '% pour le test 6.')
+# La precision avec gini est de  % pour le test 6 avec gini.
+# La precision avec entropy est de  % pour le test 6 avec entropy.
+
